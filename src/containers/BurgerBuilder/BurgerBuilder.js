0cced7be193c07893bc6f988694cdff9a9c036f2
Micah Joel <justmic007@gmail.com>
2018-07-30 12:44:44 +0100

    added remove ingredient and added a disabled property

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,17 +41,39 @@ class BurgerBuilder extends Component {
 
   }
 
-  removeIngredienthandler = (type) => {
-    const updatedCount = updatedCount - 1;
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+      if (oldCount <=0 ) {
+        return;
+      }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+
 
   }
 
   render() {
+    const disabledInfo = {
+        ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    // {salad: false, meat: false; cheese: false, bacon: false ...}
     return(
       <Aux>
           <Burger ingredients={this.state.ingredients}/>
           <BuildControls
-              ingredientAdded={this.addIngredientHandler}/>
+              ingredientAdded={this.addIngredientHandler}
+              ingredientRemoved={this.removeIngredientHandler}
+              disabled={disabledInfo}/>
       </Aux>
     );
   }
