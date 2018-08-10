16f1449b272b97ef2231f6d760a903b7012cfcf5
Micah Joel <justmic007@gmail.com>
2018-08-10 10:54:58 +0100

    backdrop behind the sidebar and also able to open it dynamically

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-      <Toolbar />
-      <SideDrawer />
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-      <main className={classes.Content}>
-          {props.children}
-      </main>
-  </Aux>
-);
+  sideDrawerCloseHandler = () => {
+      this.setState({showSideDrawer: false});
+  }
 
-export default layout;
+  render () {
+    return (
+      <Aux>
+          <Toolbar />
+          
+          <SideDrawer
+            open={this.state.showSideDrawer}
+            closed={this.sideDrawerCloseHandler}
+          />
+
+          <main className={classes.Content}>
+              {this.props.children}
+          </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout;
